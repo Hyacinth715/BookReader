47634336249ad7116c880dc6b13b84b6702a444e
Hyacinth715 <372609033@qq.com>
2021-06-09 16:41:12 +0800

    更新书城

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect:'/ebook'
+      redirect:'/store'
     },
     {
       path:'/ebook',
@@ -19,6 +19,26 @@ export default new Router({
           component:() => import('./components/ebook/EbookReader.vue')
         }
       ]
-    }   
+    },
+    {
+      path:'/store',
+      component:() => import('./views/store/index.vue'),
+      redirect:'/store/home',
+      children:[
+        {
+          path:'home',
+          component:() => import('./views/store/StoreHome.vue')
+        },
+        {
+          path: 'list',
+          component: () => import('./views/store/StoreList.vue')
+        },
+        {
+          path: 'detail',
+          component: () => import('./views/store/StoreDetail.vue')
+        }
+      ]
+    }
+
   ]
 })
